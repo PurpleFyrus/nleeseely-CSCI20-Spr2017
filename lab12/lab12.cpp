dec658f25805d859c4ddfc7e97a15042a4474b1b
Nick L S <nleeseely001@student.butte.edu>
2017-01-31 22:51:31 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,36 @@
+// Created By: Nick LeeSeely
+// Created on: January 31st 2017
+/* The following is text art for a lab for class CSC120 */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout <<"       0000000000        " << endl;
+    cout <<"      000000000000       " << endl;
+    cout <<"     00000000000000      " << endl;
+    cout <<"    0000000  0000 00     " << endl;
+    cout <<"   0000000 00 0000 00    " << endl;
+    cout <<"  0000000000000000000    " << endl;
+    cout <<" 000000000    000   0    " << endl;
+    cout <<" 0            000   0    " << endl;
+    cout <<" 0 0000000          00   " << endl;
+    cout <<" 000000000    000   00   " << endl;
+    cout <<" 000000000    0000  00   " << endl;
+    cout <<" 0000000000  000000 0    " << endl;
+    cout <<"  00000 00000000 0000    " << endl;
+    cout <<"   000 00000000   000    " << endl;
+    cout <<"    000 0000000 0 000    " << endl;
+    cout <<"    0000 000000000000    " << endl;
+    cout <<"     0000                " << endl;
+    cout <<"     0000  000 0 0 00    " << endl;
+    cout <<"     0000   00 0 0 00    " << endl;
+    cout <<"     0000    0 0 0 00    " << endl;
+    cout <<"     0000                " << endl;
+    cout <<"     00000   0 0 0 00    " << endl;
+    cout <<"      00000  0 0 0 00    " << endl;
+    cout <<"       00000             " << endl;
+    cout <<"        0000000000000    " << endl;
+    cout <<"         0000000000000   " << endl;
+}
